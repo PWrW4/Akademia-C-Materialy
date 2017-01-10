54d69ac4a299da37edb128634c7a2a8d4e2410c9
wojtasiq-ideapad310 <wojtek.wroclaw@hotmail.com>
2017-01-10 13:12:29 +0100

    Akademia7

---
+++ AkademiaCSharp7/Entity/Entity/Migrations/201612201902447_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace Entity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MyTasks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        DueDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.MyTasks");
+        }
+    }
+}
